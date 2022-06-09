4847c6dc125b00f20452a45e140d3d51392f416e
abhibamnote <79627580+abhibamnote@users.noreply.github.com>
2022-06-09 15:49:17 +0530

    Changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-function navBtnClicked(){
-    document.getElementById('NavlinksHere').classList.toggle('make-display-non');
+function navBtnClicked() {
+  document.getElementById("NavlinksHere").classList.toggle("make-display-non");
 }
+
+const left = document.querySelector(".split-left");
+const right = document.querySelector(".split-right");
+const container = document.querySelector(".container-split");
+
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+);
